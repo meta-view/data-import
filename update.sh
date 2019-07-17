5141d3c54c44c533e6ddc83a438d48655fa9078a
Philipp Haussleiter <philipp@haussleiter.de>
2019-07-17 23:57:51 +0200

    updating templates and assets for tabler 0.0.33

--- update.sh
+++ update.sh
@@ -2,9 +2,11 @@
 
 TABLER_VERSION=0.0.33
 
-mkdir -p assets/tabler tmp
-rm -Rf tmp/tabler
+mkdir -p assets/tabler assets/js tmp
+rm -Rf tmp/tabler assets/tabler
 
 wget -c -N http://maven.javastream.de/org/webjars/tabler/${TABLER_VERSION}/tabler-${TABLER_VERSION}.jar -O tmp/tabler-${TABLER_VERSION}.jar
 unzip tmp/tabler-${TABLER_VERSION}.jar -d tmp/tabler
 cp -R tmp/tabler/META-INF/resources/webjars/tabler/${TABLER_VERSION}/assets/ assets/tabler/
+mv assets/tabler/js/core.js assets/js/
+mv -f assets/tabler/js/vendors assets/js/
