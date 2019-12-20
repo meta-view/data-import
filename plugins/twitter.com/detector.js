d40692058a185777fccc9f403f94357676985d78
philipp <philipp@haussleiter.de>
2019-12-20 01:11:14 +0100

    updating plugin marker for JS logs

--- plugins/twitter.com/detector.js
+++ plugins/twitter.com/detector.js
@@ -1,9 +1,9 @@
 (function(){
     console.log("\tJS\t[twitter.com] Checking payload " + payloadPath);
     files = getFiles()
-    console.log("loading " + files.length + " files");
+    console.log("\tJS\t[twitter.com] loading " + files.length + " files");
     for (i in files) {
-        console.log("ContentType of " + files[i] + " is " + getContentType(files[i]));
+        console.log("\tJS\t[twitter.com] ContentType of " + files[i] + " is " + getContentType(files[i]));
     }
     return "0.50";
 })();
