bbc7b861342fa988db382e529288061751378f30
Philipp Haussleiter <philipp@haussleiter.de>
2019-12-29 15:19:48 +0100

    updating default presenter

--- plugins/testProvider/presenter.js
+++ plugins/testProvider/presenter.js
@@ -25,12 +25,31 @@
         out = '';
         out += '<div class="card">';
         out += '    <div class="card-body d-flex flex-column">';
-        out += '        <div class="text-muted">';
-        out += '            <pre>{{post-content}}</pre>';
+        out += '        <div>';
+        out += '            <a href="/?provider={{provider}}" class="text-default">{{provider}}</a>/';
+        out += '            <a href="/?table={{table}}" class="text-default">{{table}}</a>';
         out += '        </div>';
+        out += '        <div class="text-muted"><small>{{post-content}}</small></div>';
+        out += '        <div class="d-flex align-items-center pt-5 mt-auto">';
+        out += '            <div>';
+        out += '                <small class="d-block">imported <span class="text-muted">{{imported}}</span></small>';
+        out += '                <small class="d-block">created <span class="text-muted">{{created}}</span></small>';
+        out += '                <small class="d-block">updated <span class="text-muted">{{updated}}</span></small>';
+        out += '            </div>';
+        out += '       </div>';
         out += '    </div>';
         out += '</div>';
-        out = out.replace("{{post-content}}", entry["content"].replace(",", ",\n"));
+        out = replaceAll(out, "{{table}}", entry["table"]);
+        out = replaceAll(out, "{{provider}}", entry["provider"]);
+        out = out.replace("{{imported}}", entry["imported"]);
+        out = out.replace("{{updated}}", entry["updated"]);
+        out = out.replace("{{created}}", entry["created"]);
+        out = out.replace("{{post-content}}", 
+            replaceAll(entry["content"],",",",\n"));
         return out;
     }
+
+    function replaceAll(target, search, replacement) {
+        return target.split(search).join(replacement);
+    };
 })();
